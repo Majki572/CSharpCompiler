c6dbb03628bd11125a7e56faeb8e71c0f0f36c4d
Mikołaj <mrefe12345@gmail.com>
2024-04-08 15:55:42 +0200

    add lexer and parser to csproj, change namespaces

--- Compiler/Grammar/Program.cs
+++ Compiler/Grammar/Program.cs
@@ -1,7 +1,7 @@
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 
-namespace AntlrCSharp;
+namespace Compiler.Grammar;
 
 public class Program()
 {
@@ -9,17 +9,17 @@ public class Program()
     {
         var currentDirectory = Directory.GetCurrentDirectory();
         var input = File.ReadAllText(@"langX.test");
-        
+
         AntlrInputStream inputStream = new AntlrInputStream(input);
         LangXLexer lexer = new LangXLexer(inputStream);
         CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
         LangXParser parser = new LangXParser(commonTokenStream);
         IParseTree parseTree = parser.start();
-        
+
         var listener = new BasicLangXListener();
         ParseTreeWalker.Default.Walk(listener, parseTree);
-        
+
         // create file from generated code
         File.WriteAllText("output.ll", listener.GenerateCode());
-    }   
+    }
 }
